e86e30802540b8d1090aed547ae6d0443bc9ce1b
a <a@a-PC>
2018-01-09 10:32:19 +0900

    1

--- src/main/java/org/oraclejava/spring/sample/HelloController.java
+++ src/main/java/org/oraclejava/spring/sample/HelloController.java
@@ -1,13 +1,20 @@
 package org.oraclejava.spring.sample;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
+	
 	@RequestMapping("/hello")
 	public String helloWorld() {
+		logger.info("----------hello world-----------");
 		return "hello";
 	}
 }
